a40ab3dfafd82bb26fe194ada4e7b16029634f0c
mnaumoski <naumoski.marija@gmail.com>
2016-11-07 09:02:56 -0500

    seeds and schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,16 +4,8 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(255) NOT NULL,
+burger varchar(255) NOT NULL,
 devoured boolean NOT NULL,
-date 
+burger_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (id)
 );
-
--- INSERT INTO movies (movie) VALUES ('Legend of the Fall');
-
--- date: a TIMESTAMP.
-
--- Still in the db folder, create a seeds.sql file. 
--- In this file, write insert queries to populate the 
--- burgers table with at least three entries.
